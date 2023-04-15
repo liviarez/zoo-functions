cd7e0619b0557ff4a9ef179115b8a57b3ee75f47
liviarez <mrezende.livia@gmail.com>
2023-04-15 16:13:49 -0300

    5. Implemente a função countAnimals.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const getSpeciesByName = (name) => species
+    .find((specie) => specie.name === name);
+  const countAllAnimals = () => species
+    .reduce((acc, { name, residents }) => ({ ...acc, [name]: residents.length }), {});
+
+  if (!animal) {
+    return countAllAnimals();
+  }
+
+  const [animalName, animalSex] = Object.values(animal);
+
+  if (!animalSex) {
+    return getSpeciesByName(animalName).residents.length;
+  }
+
+  const animals = getSpeciesByName(animalName).residents.filter((res) => res.sex === animalSex);
+
+  return animals.length;
 };
 
 module.exports = countAnimals;
