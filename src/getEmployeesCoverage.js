4e67d2ba83157b6641b02734ea37e49801a5c4c6
liviarez <mrezende.livia@gmail.com>
2023-04-24 10:15:29 -0300

    10. Implemente a função getOldestFromFirstSpecies.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,32 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const employeeCoverage = employees.map((info) => (
+  {
+    id: info.id,
+    fullName: `${info.firstName} ${info.lastName}`,
+    species: info.responsibleFor.map((id) => (
+      species.find((speId) => (speId.id === id)).name
+    )),
+    locations: info.responsibleFor.map((id) => (
+      species.find((specieLocation) => (specieLocation.id === id)).location
+    )),
+  }
+));
+
+const getEmployeesCoverage = (obj) => {
+  const employee = obj && employeeCoverage.find((info) => (
+    obj.id ? info.id === obj.id : info.fullName.includes(obj.name)
+  ));
+
+  if (employee) {
+    return employee;
+  }
+
+  if (obj) {
+    throw new Error('Informações inválidas');
+  }
+
+  return employeeCoverage;
 };
 
 module.exports = getEmployeesCoverage;
